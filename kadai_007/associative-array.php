f52d082f3639bf9c3f7f9b68f06cc52c60733505
tankuuuumi <150252496+tankuuuumi@users.noreply.github.com>
2024-01-11 17:49:42 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        //値を代入
+        $associative_arry = ['name' => 'onion','price' => 200, 'weight' => '160'];
+
+        //値を出力
+        print_r($associative_arry);
+
+        ?>
+    </p>
+</body>
+</html>
